0eaf9333f2f9a46f0aaf5359483f52bfb0b2abe4
Vladyslav Pikus <vpikus@eisgroup.com>
2021-12-03 16:41:05 +0200

    EPBDS-11381 groups must not be mandatory

--- STUDIO/org.openl.rules.webstudio/test/org/openl/rules/rest/validation/MockConfiguration.java
+++ STUDIO/org.openl.rules.webstudio/test/org/openl/rules/rest/validation/MockConfiguration.java
@@ -1,11 +1,12 @@
 package org.openl.rules.rest.validation;
 
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import javax.annotation.PostConstruct;
 
-import org.openl.config.InMemoryProperties;
 import org.openl.rules.webstudio.security.CurrentUserInfo;
-import org.openl.rules.webstudio.service.AdminUsers;
-import org.openl.rules.webstudio.service.ExternalGroupService;
 import org.openl.rules.webstudio.service.GroupManagementService;
 import org.openl.rules.webstudio.service.UserManagementService;
 import org.openl.rules.webstudio.web.Props;
@@ -19,10 +20,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @Configuration
 @ComponentScan(basePackages = "org.openl.rules.rest.validation")
@@ -75,20 +72,4 @@ public class MockConfiguration {
     public GroupManagementService groupManagementService() {
         return mock(GroupManagementService.class);
     }
-
-    @Bean
-    public ExternalGroupService externalGroupService() {
-        return mock(ExternalGroupService.class);
-    }
-
-    @Bean
-    public AdminUsers adminUsers() {
-        return mock(AdminUsers.class);
-    }
-
-    @Bean
-    public InMemoryProperties properties() {
-        return mock(InMemoryProperties.class);
-    }
-
 }
